bd6fa4c41fca1212a27e3e94834c282f7b703add
Kareem Morsy <kreem.morsy@hotmail.com>
2021-11-19 17:00:56 +0200

    Finished offer and request classes

--- src/Offer.java
+++ src/Offer.java
@@ -1,3 +1,16 @@
 public class Offer
 {
+    private Request request;
+    private float offerPrice;
+    private Driver driver;
+
+    private final RideManager rideManager = RideManager.getInstance();
+
+    public void accept() {
+        rideManager.setOfferAccepted(this, true);
+    }
+
+    public void reject() {
+        rideManager.setOfferAccepted(this, false);
+    }
 }
