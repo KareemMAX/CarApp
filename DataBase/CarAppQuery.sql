2f082c4bd34e8efb5eb3f44f8cb1c2ebe76992a3
Kareem Morsy <kreem.morsy@hotmail.com>
2021-11-22 01:55:57 +0200

    Finished the database class with some schema modifications

--- DataBase/CarAppQuery.sql
+++ DataBase/CarAppQuery.sql
@@ -1,69 +1,86 @@
+CREATE DATABASE car_app;
+GO;
+
+USE car_app;
+
 CREATE TABLE admin
 (
-    userid    int,
-    password  varchar(100),
+    user_id   int IDENTITY (1,1),
     username  varchar(100),
+    password  varchar(100),
     suspended bit,
-    PRIMARY KEY (userid)
+    PRIMARY KEY (user_id)
 );
-CREATE TABLE request
-(
-    ride_id     int,
-    source      varchar(100),
-    destination varchar(100),
-    PRIMARY KEY (ride_id),
 
-);
 CREATE TABLE customer
 (
-    customer_id  int,
-    ride_id      int,
-    password     varchar(100),
+    customer_id  int IDENTITY (1,1),
     username     varchar(100),
+    password     varchar(100),
     email        varchar(100),
-    suspended    bit,
     phone_number varchar(100),
+    suspended    bit,
     PRIMARY KEY (customer_id),
-    FOREIGN KEY (ride_id) REFERENCES request (ride_id)
 );
 
 CREATE TABLE driver
 (
-    driver_id int,
-    password  varchar(100),
-    username  varchar(100),
-    email     varchar(100),
-    phone     varchar(100),
-    license   varchar(100),
-    suspended bit,
-    verified  bit,
+    driver_id   int IDENTITY (1,1),
+    username    varchar(100),
+    password    varchar(100),
+    email       varchar(100),
+    phone       varchar(100),
+    national_id varchar(100),
+    license     varchar(100),
+    suspended   bit,
+    verified    bit,
     PRIMARY KEY (driver_id),
-
 );
+
 CREATE TABLE rate
 (
-    rate_id    int,
+    user_id    int,
     driver_id  int,
-    userid     int,
-    PRIMARY KEY (rate_id),
     rate_value float,
+    PRIMARY KEY (user_id, driver_id),
     FOREIGN KEY (driver_id) REFERENCES driver (driver_id),
-    FOREIGN KEY (userid) REFERENCES customer (customer_id)
+    FOREIGN KEY (user_id) REFERENCES customer (customer_id)
 );
+
+CREATE TABLE request
+(
+    request_id  int IDENTITY (1,1),
+    source      varchar(100),
+    destination varchar(100),
+    user_id     int,
+    PRIMARY KEY (request_id),
+    FOREIGN KEY (user_id) REFERENCES customer (customer_id)
+);
+
 CREATE TABLE offer
 (
-    offer_id  int,
+    offer_id  int IDENTITY (1,1),
     driver_id int,
     ride_id   int,
     PRIMARY KEY (offer_id),
     price     int,
     FOREIGN KEY (driver_id) REFERENCES driver (driver_id),
-    FOREIGN KEY (ride_id) REFERENCES request (ride_id)
+    FOREIGN KEY (ride_id) REFERENCES request (request_id)
 );
+
 CREATE TABLE favourite_places
 (
     favourite_place varchar(100),
     driver_id       int,
     PRIMARY KEY (favourite_place, driver_id),
     FOREIGN KEY (driver_id) REFERENCES driver (driver_id),
+);
+
+CREATE TABLE past_rides
+(
+    user_id  int,
+    offer_id int,
+    PRIMARY KEY (user_id, offer_id),
+    FOREIGN KEY (user_id) REFERENCES customer (customer_id),
+    FOREIGN KEY (offer_id) REFERENCES offer (offer_id)
 );
