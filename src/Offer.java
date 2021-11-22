0ec0c3372f295f4c80724d3d782627b8d392e280
Kareem Morsy <kreem.morsy@hotmail.com>
2021-11-22 02:06:12 +0200

    Documented Offer, Request and made some fixes in Database

--- src/Offer.java
+++ src/Offer.java
@@ -1,3 +1,8 @@
+/**
+ * A full offer provided from the driver to a user {@link Request}.
+ *
+ * @author Kareem Morsy
+ */
 public class Offer {
     private final Request request;
     private final float offerPrice;
@@ -5,28 +10,52 @@ public class Offer {
 
     private final RideManager rideManager = RideManager.getInstance();
 
+    /**
+     * Offer default constructor
+     * @param request The original {@link Request} that the offer is for
+     * @param offerPrice The offer proposed price
+     * @param driver The {@link Driver} object offering
+     */
     public Offer(Request request, float offerPrice, Driver driver) {
         this.request = request;
         this.offerPrice = offerPrice;
         this.driver = driver;
     }
 
+    /**
+     * The original {@link Request} that the offer is for
+     * @return The original {@link Request} that the offer is for
+     */
     public Request getRequest() {
         return request;
     }
 
+    /**
+     * The offer proposed price
+     * @return The offer proposed price
+     */
     public float getOfferPrice() {
         return offerPrice;
     }
 
+    /**
+     * The {@link Driver} object offering
+     * @return The {@link Driver} object offering
+     */
     public Driver getDriver() {
         return driver;
     }
 
+    /**
+     * Accepts the offer while rejecting any other existing offers to the same {@link Request} object
+     */
     public void accept() {
         rideManager.setOfferAccepted(this, true);
     }
 
+    /**
+     * Rejects the offer and clear it from memory/storage
+     */
     public void reject() {
         rideManager.setOfferAccepted(this, false);
     }
--- src/Request.java
+++ src/Request.java
@@ -1,27 +1,63 @@
 import java.util.List;
 
+/**
+ * A request from the {@link Customer} to have a ride from a specific source to a specific destination.
+ */
 public class Request {
     private final RideManager rideManager = RideManager.getInstance();
-    private String source;
-    private String destination;
-    private Customer user;
+    private final String source;
+    private final String destination;
+    private final Customer user;
 
+    /**
+     * Request default constructor
+     * @param source The source area of the request
+     * @param destination The destination area of the request
+     * @param user The requested {@link Customer}
+     */
+    public Request(String source, String destination, Customer user) {
+        this.source = source;
+        this.destination = destination;
+        this.user = user;
+    }
+
+    /**
+     * The source area of the request
+     * @return The source area of the request
+     */
     public String getSource() {
         return source;
     }
 
+    /**
+     * The destination area of the request
+     * @return The destination area of the request
+     */
     public String getDestination() {
         return destination;
     }
 
+    /**
+     * The requested {@link Customer}
+     * @return The requested {@link Customer}
+     */
     public Customer getUser() {
         return user;
     }
 
+    /**
+     * Gets all the offers offered to this request
+     * @return A list of {@link Offer} that contains a price and a {@link Driver}
+     */
     public List<Offer> getAllOffers() {
         return rideManager.listOffers(this);
     }
 
+    /**
+     * Purpose an {@link Offer} to this request
+     * @param driver The {@link Driver} offering the offer
+     * @param price The price purposed
+     */
     public void makerOffer(Driver driver, float price) {
         rideManager.makeOffer(driver, this, price);
     }
