db2251b610898963014330b167485821b500485f
Khaled-Waled <khaledw.hamed001@gmail.com>
2021-12-24 20:52:50 +0200

    Added base structure of the event system

---
+++ src/model/EventManager.java
@@ -0,0 +1,25 @@
+package model;
+
+import controller.Event;
+import controller.Request;
+
+import java.util.ArrayList;
+
+public class EventManager
+{
+    public void receiveEvent(Event event)
+    {
+
+    }
+    public ArrayList<Event> getAllEvents()
+    {
+
+        return null;
+    }
+    public ArrayList<Event> getEventsForRide(Request request)
+    {
+
+
+        return null;
+    }
+}
